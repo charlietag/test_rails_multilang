638ea5597a6f2093971cee4f92607f5de9e4a3cc
Charlie Hsieh <charlieofficial@gmail.com>
2020-08-11 14:53:05 +0800

    setting locales related using initializers , rather than config/application.rb

---
+++ config/initializers/locales.rb
@@ -0,0 +1,16 @@
+# --------------------------------------------------------------------------------------
+# Ref. https://yalantis.com/blog/multi-language-ruby-on-rails-app-development/
+# Easier way to classify locale yml files
+# --------------------------------------------------------------------------------------
+
+# Where the I18n library should search for translation files
+
+#I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+# Set default locale to something other than :en
+
+#I18n.default_locale = :en
+
+# Whitelist locales available for the application
+
+#I18n.available_locales = [:en, :"zh-TW"]
