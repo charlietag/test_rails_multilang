ca1108e6290ecbe0389f3e09a93e914d5a043008
Charlie Hsieh <charlieofficial@gmail.com>
2020-08-14 13:38:58 +0800

    change sample code to be the same as rails guide

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -6,7 +6,8 @@
 
 # Where the I18n library should search for translation files
 
-I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+#I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
 # Set default locale to something other than :en
 
